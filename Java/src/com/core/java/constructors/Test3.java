2c0d680d211c5605131aeca17d2f32394ec5ef30
vinodsamudrala <vinod.dec9@gmail.com>
2016-06-08 10:42:55 +0530

    8-6-2016

---
+++ Java/src/com/core/java/constructors/Test3.java
@@ -0,0 +1,22 @@
+package com.core.java.constructors;
+
+public class Test3 {
+
+	int x;
+
+	/*public static Test3() {
+		
+	}
+	*/
+	
+	/*public final Test3() {
+	
+	}
+	*/
+	private Test3(int x) {
+		
+	}
+	public static void main(String[] args) {
+		Test3 t =new Test3(10);
+	}
+}
